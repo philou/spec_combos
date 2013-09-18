182a005b682a5ab8de5357d12e41a638e23e41f3
Philou <philippe.bourgau@free.fr>
2013-09-18 06:37:53 +0200

    Detailed error messages for 'all' matcher

--- lib/spec_combos/all_matcher.rb
+++ lib/spec_combos/all_matcher.rb
@@ -1,12 +1,71 @@
 # Matcher to verify that all items match something else
-RSpec::Matchers.define :all_ do |item_matcher|
 
-  match do |actual_items|
-    actual_items.all? { |item| item_matcher.matches?(item)}
+module SpecCombos
+  class AllMatcher
+
+    def initialize(&item_matcher_proc)
+      @item_matcher_proc = item_matcher_proc
+    end
+
+    def matches?(actual_items)
+      perform_matches(actual_items)
+      failures(true).empty?
+    end
+
+    def description
+      "all #{@item_matcher_proc.call.description}"
+    end
+
+    def failure_message_for_should
+      failure_message(SHOULD, "expected #{actual_items} to #{description}, but the following items were not:")
+    end
+
+    def failure_message_for_should_not
+      failure_message(SHOULD_NOT, "expected #{actual_items} not to #{description}, but the following items were:")
+    end
+
+    private
+
+    SHOULD = true
+    SHOULD_NOT = false
+
+    def perform_matches(actual_items)
+      @match_details = actual_items.each_with_index.map do |item, index|
+        matcher = @item_matcher_proc.call
+        { index: index,
+          item: item,
+          matcher: matcher,
+          match: matcher.matches?(item)}
+      end
+    end
+    def failure_message(expected_match, summary)
+      ([summary] + explanations(expected_match)).join("\n")
+    end
+    def actual_items
+      @match_details.map {|detail| detail[:item]}
+    end
+    def explanations(expected_match)
+      failures(expected_match).map {|failure| explain(failure)}
+    end
+    def failures(expected_match)
+      @match_details.find_all {|detail| detail[:match] != expected_match}
+    end
+    def explain(failure)
+        "  ##{failure[:index]}, #{failure[:item]} : #{message(failure)}"
+    end
+    def message(failure)
+      failure[:matcher].send(message_selector(failure))
+    end
+    def message_selector(failure)
+      if failure[:match]
+        :failure_message_for_should_not
+      else
+        :failure_message_for_should
+      end
+    end
   end
+end
 
-  description do
-    "#{item_matcher.description} to be true for all the items"
-  end
-
+def all_(&item_matcher_proc)
+  SpecCombos::AllMatcher.new(&item_matcher_proc)
 end
--- spec/lib/spec_combos/all_matcher_spec.rb
+++ spec/lib/spec_combos/all_matcher_spec.rb
@@ -2,10 +2,36 @@ require 'spec_combos/all_matcher'
 
 describe 'all matcher' do
   it 'matches collections with all matching elements' do
-    expect([1,3,5]).to all_ be_odd
+    expect([1,3,5]).to all_ { be_odd }
   end
 
   it 'does not match collections where not all elements match' do
-    expect([2,5,6]).not_to all_ be_even
+    expect([3,5,6]).not_to all_ { be_odd }
+  end
+
+  it "has a straigtforward description" do
+    matcher = all_ { be_odd }
+
+    expect(matcher.description).to eq "all #{be_odd.description}"
+  end
+
+  context "error messages" do
+    before :each do
+      @matcher = all_ { be_odd }
+      @matcher.matches?([1,2,3])
+    end
+
+    it 'has an explicit should message' do
+      expect(@matcher.failure_message_for_should).to eq(
+        "expected [1, 2, 3] to all be odd, but the following items were not:\n" +
+        "  #1, 2 : expected odd? to return true, got false")
+    end
+
+    it 'has an explicit should not message' do
+      expect(@matcher.failure_message_for_should_not).to eq(
+        "expected [1, 2, 3] not to all be odd, but the following items were:\n" +
+        "  #0, 1 : expected odd? to return false, got true\n" +
+        "  #2, 3 : expected odd? to return false, got true")
+    end
   end
 end
